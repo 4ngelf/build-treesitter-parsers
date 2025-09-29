85cf37e6de2a640e578edf27bd5247ec2780a26e
Angel Fuenmayor <32184965+4ngelf@users.noreply.github.com>
2025-09-29 10:43:26 -0400

    feat: select which parsers to compile

--- build.zig
+++ build.zig
@@ -37,17 +37,45 @@ pub fn build(b: *std.Build) void {
         .generate_parser_tool = generate_parser_tool,
     };
 
+    const parser_option = b.option([]const ParserName, "parser", "parsers to compile separated by commas or `all`. [default: all]");
+    const parser_set: std.EnumSet(ParserName) =
+        if (parser_option) |selection| set: {
+            var set: std.EnumSet(ParserName) = .initMany(selection);
+            if (set.contains(.all)) set.toggleAll();
+            break :set set;
+        } else .initFull();
+
     inline for (parsers) |parser| {
-        const parser_lib = parser.build(b, &config);
-        const parser_install = b.addInstallArtifact(parser_lib, .{
-            .dest_dir = .{ .override = .{ .custom = "parser" } },
-            .pdb_dir = .disabled,
-            .implib_dir = .disabled,
-            .dest_sub_path = @tagName(parser.name) ++ ".so",
-        });
-        b.getInstallStep().dependOn(&parser_install.step);
+        if (parser_set.contains(parser.name)) {
+            const parser_lib = parser.build(b, &config);
+            const parser_install = b.addInstallArtifact(parser_lib, .{
+                .dest_dir = .{ .override = .{ .custom = "parser" } },
+                .pdb_dir = .disabled,
+                .implib_dir = .disabled,
+                .dest_sub_path = @tagName(parser.name) ++ ".so",
+            });
+            b.getInstallStep().dependOn(&parser_install.step);
+        }
     }
 }
+
+const ParserName = blk: {
+    const len = parsers.len + 1;
+    var names: [len]std.builtin.Type.EnumField = undefined;
+    names[0] = .{ .name = "all", .value = 0 };
+    for (parsers, 1..) |parser, i| {
+        names[i] = .{ .name = @tagName(parser.name), .value = i };
+    }
+    break :blk @Type(.{ .@"enum" = .{
+        .tag_type = @Type(.{ .int = .{
+            .bits = @typeInfo(usize).int.bits - @clz(len),
+            .signedness = .unsigned,
+        } }),
+        .fields = &names,
+        .decls = &.{},
+        .is_exhaustive = false,
+    } });
+};
 
 const std = @import("std");
 const Parser = @import("build/Parser.zig");
